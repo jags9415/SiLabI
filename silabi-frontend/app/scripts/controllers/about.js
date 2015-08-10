e76b8746b9aca5c3fdfe493782d27c466862ef60
Emmurillo <emmanuel-ms@hotmail.com>
2015-08-10 17:52:54 -0600

    Add project's scaffolding

---
+++ silabi-frontend/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name silabiFrontendApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the silabiFrontendApp
+ */
+angular.module('silabiFrontendApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ silabi-frontend/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name silabiFrontendApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the silabiFrontendApp
+ */
+angular.module('silabiFrontendApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
