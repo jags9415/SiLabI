4ec0dd12962a9772c0823afc54d7bd6746834df6
Leonardo Madrigal <lmadrigal09@gmail.com>
2015-08-22 23:02:47 -0600

    Administrators home view implemented.

--- frontend/app/scripts/administrators/admin.controller.js
+++ frontend/app/scripts/administrators/admin.controller.js
@@ -4,15 +4,40 @@
     angular
         .module('silabi')
         .controller('AdminController', AdminController);
-        AdminController.$inject = ['$scope', '$routeParams'];
+        AdminController.$inject = ['$scope', '$routeParams', 'AdminService'];
 
-    function AdminController($scope, $routeParams) {
+    function AdminController($scope, $routeParams, AdminService) {
       
-      init();
+      	init();
+      	loadPage(1);
+    	
+    	$scope.loadPage = loadPage;
+
+    	 function loadPage(number)
+    	{
+    		var pnumber = $scope.pageNumber + number;
+    		if(pnumber > 0 && pnumber <= $scope.toalPages)
+    		{
+	    		AdminService.getAdmins($scope.pageNumber + number, "a"/*access_token*/ ).
+	    		then(function(response)
+			        {
+						$scope.adminsArray = response.results;
+						$scope.pageNumber = pnumber;
+						$scope.toalPages = response.total_pages;
+					},
+					function(error)
+			        {
+						LoggerService.logError(error);
+					}
+				);
+	    	}
+    	};
 
     	function init()
     	{
     		$scope.adminsArray = [];
+    		$scope.pageNumber = 0;
+    		$scope.toalPages = 1;
     	}
     }
 })();
