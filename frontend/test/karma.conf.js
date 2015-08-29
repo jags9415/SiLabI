0bcb7a23958775e68c0cfe846fb879f75b781d97
Jose Garcia <jags9415@gmail.com>
2015-08-28 20:41:29 -0600

    Add ui-mask

--- frontend/test/karma.conf.js
+++ frontend/test/karma.conf.js
@@ -35,6 +35,7 @@ module.exports = function(config) {
       'bower_components/angular-jwt/dist/angular-jwt.js',
       'bower_components/angular-bootstrap-show-errors/src/showErrors.js',
       'bower_components/ngstorage/ngStorage.js',
+      'bower_components/angular-ui-mask/dist/mask.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
