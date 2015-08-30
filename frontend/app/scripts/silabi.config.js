8b6c23fb0549fc1c822fb0398a92459b72ba8d08
Jose Garcia <jags9415@gmail.com>
2015-08-30 14:01:54 -0600

    Add validation to student forms

---
+++ frontend/app/scripts/silabi.config.js
@@ -0,0 +1,26 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('silabi').config(['showErrorsConfigProvider', function(showErrorsConfigProvider) {
+      showErrorsConfigProvider.showSuccess(true);
+    }]);
+
+    toastr.options = {
+      "closeButton": true,
+      "debug": false,
+      "newestOnTop": true,
+      "progressBar": false,
+      "positionClass": "toast-top-center",
+      "preventDuplicates": true,
+      "onclick": null,
+      "showDuration": "300",
+      "hideDuration": "1000",
+      "timeOut": "5000",
+      "extendedTimeOut": "1000",
+      "showEasing": "swing",
+      "hideEasing": "linear",
+      "showMethod": "fadeIn",
+      "hideMethod": "fadeOut"
+    }
+})();
--- frontend/app/scripts/silabi.constants.js
+++ frontend/app/scripts/silabi.constants.js
@@ -5,22 +5,4 @@
         .module('silabi')
         .constant('API_URL','http://localhost/api/v1')
         .constant('toastr', toastr);
-
-    toastr.options = {
-      "closeButton": true,
-      "debug": false,
-      "newestOnTop": true,
-      "progressBar": false,
-      "positionClass": "toast-top-center",
-      "preventDuplicates": true,
-      "onclick": null,
-      "showDuration": "300",
-      "hideDuration": "1000",
-      "timeOut": "5000",
-      "extendedTimeOut": "1000",
-      "showEasing": "swing",
-      "hideEasing": "linear",
-      "showMethod": "fadeIn",
-      "hideMethod": "fadeOut"
-    }
 })();
