32cab189f7e26854ee160dd4a52156b572912200
Jose Garcia <jags9415@gmail.com>
2015-08-30 19:20:12 -0600

    Update professor detail. Add validation

--- frontend/app/scripts/operator/professors/professor.detail.controller.js
+++ frontend/app/scripts/operator/professors/professor.detail.controller.js
@@ -8,105 +8,51 @@
 
     function ProfessorsDetailController($routeParams, ProfessorsService, $route, $location, MessageService) {
         var vm = this;
-      	activate();
+        vm.genders = ["Masculino", "Femenino"];
+        vm.username = $routeParams.username;
+        vm.update = updateProfessor;
+        vm.delete = deleteProfessor;
 
-        vm.modifyProfessor = modifyProfessor;
-        vm.deleteProfessor = deleteProfessor;
-      	
-        function  checkProfessorModifyInput() 
+        activate();
+
+        function activate()
+      	{
+          ProfessorsService.GetOne(vm.username).
+          then(handleGetSuccess)
+          .catch(handleError);
+      	}
+
+        function updateProfessor()
         {
-            if(!isNaN(vm.professorModifyName))
-            {
-                alert("Nombre incorrecto.");
-                return false;
+          if (vm.professor) {
+            if (vm.password) {
+              var hash = CryptoJS.SHA256(vm.password).toString(CryptoJS.enc.Hex);
+              vm.professor.password = hash;
             }
-            else if(!isNaN(vm.professorModifyLastName1))
-            {
-                alert("Primer Apellido incorrecto.");
-                return false;
-            }
-            else if(!isNaN(vm.professorModifyLastName2))
-            {
-                alert("Segundo Apellido incorrecto.");
-                return false;
-            }
-            else
-            {
-                return true;
-            }
+            ProfessorsService.Update(vm.professor.id, vm.professor)
+            .then(handleGetSuccess)
+            .catch(handleError);
+          }
         }
 
-        function retrieveProfessor()
-        {
-            vm.professorUserName =  $routeParams.username;
-            ProfessorsService.GetOne(vm.professorUserName).
-            then(function(response)
-            {
-                vm.professor = response;
-                vm.professorid = vm.professor.id;
-                console.log(response);
-            },
-            function(error)
-            {
-                alert("Error al obtener datos de docente. "+error.description);
-            });
-        }
-
-        function modifyProfessor()
-        {
-            if(checkProfessorModifyInput())
-            {
-                var hash = CryptoJS.SHA256(vm.professor.password).toString(CryptoJS.enc.Hex);
-                var jsonObject =
-                {
-                    "professor":
-                    {
-                        "email": vm.professor.email,
-                        "last_name_1": vm.professor.last_name_1,
-                        "last_name_2": vm.professor.last_name_2,
-                        "password": hash,
-                        "phone": vm.professor.phone
-                    },
-                    "access_token":""
-                };
-                ProfessorsService.Update(vm.professorid, jsonObject)
-                .then(handleGetSuccess)
-                .catch(handleError);
-            }
-        }
-
-        function deleteProfessor()
-        {
-            ProfessorsService.Delete(vm.professorid)
+        function deleteProfessor() {
+          if (vm.professor) {
+            ProfessorsService.Delete(vm.professor.id)
             .then(handleDeleteSuccess)
             .catch(handleError);
+          }
         }
 
         function handleGetSuccess(data) {
           vm.professor = data;
-          MessageService.success("Docente actualizado.");
         }
 
         function handleDeleteSuccess() {
-          MessageService.success("Docente eliminado.");
           $location.path("/Operador/Docentes");
         }
 
         function handleError(data) {
           MessageService.error(data.description);
         }
-
-    	function activate()
-    	{
-    		vm.genders = [{"name":"Masculino"}, {"name":"Femenino"}];
-            var accessToken = -1;
-            if(typeof(sessionStorage) != 'undefined' && sessionStorage.getItem('access_token') != null)
-            {
-                accessToken = sessionStorage.getItem('access_token');
-            }
-            vm.professorid = null;
-            vm.professor = null;
-            retrieveProfessor();
-    	}
     }
 })();
--- frontend/app/scripts/professor/professor.service.js
+++ frontend/app/scripts/professor/professor.service.js
@@ -34,12 +34,12 @@
             return RequestService.post('/professors/', request);
         };
 
-        this.Update = function(id, jsonObject)
+        this.Update = function(id, professor)
         {
             var request = {};
-            request.professor = jsonObject.professor;
+            request.professor = professor;
             request.access_token = $localStorage['access_token'];
-            return RequestService.put('/professors/'+id, request);
+            return RequestService.put('/professors/' + id, request);
         };
 
         this.Delete = function(id)
@@ -48,6 +48,6 @@
             request.access_token = $localStorage['access_token'];
             return RequestService.delete('/professors/'+id, request);
         }
-        
+
     }
 })();
