b68b6b611b5734120018351056d1aaea6a55e988
José Andrés García <jags9415@gmail.com>
2015-09-03 14:00:38 -0600

    Update Gruntfile.js

--- frontend/Gruntfile.js
+++ frontend/Gruntfile.js
@@ -71,8 +71,7 @@ module.exports = function (grunt) {
     connect: {
       options: {
         port: 9000,
-        // Change this to '0.0.0.0' to access the server from outside.
-        hostname: 'localhost',
+        hostname: '0.0.0.0',
         livereload: 35729
       },
       livereload: {
@@ -319,7 +318,7 @@ module.exports = function (grunt) {
     ngtemplates: {
       dist: {
         options: {
-          module: 'silabiFrontendApp',
+          module: 'silabi-frontend',
           htmlmin: '<%= htmlmin.dist.options %>',
           usemin: 'scripts/scripts.js'
         },
