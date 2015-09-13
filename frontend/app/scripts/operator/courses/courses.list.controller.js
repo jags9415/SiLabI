f3322801ee7d86461713cad0e7460d4555c0e86e
Emmurillo <emmanuel-ms@hotmail.com>
2015-09-12 21:14:22 -0600

    Retrieve courses and search with queries for code and name

--- frontend/app/scripts/operator/courses/courses.list.controller.js
+++ frontend/app/scripts/operator/courses/courses.list.controller.js
@@ -5,17 +5,97 @@
         .module('silabi')
         .controller('CourseListController', CourseListController);
 
-    CourseListController.$inject = ['$routeParams'];
+    CourseListController.$inject = ['$location', 'CourseService', 'MessageService'];
 
-    function CourseListController($routeParams) {
+    function CourseListController($location, CourseService, MessageService) {
         var vm = this;
 
-        vm.courses = [{
-          code: "IC-3847",
-          name: "Inglés 2 para Computación"
-        },{
-          code: "IC-8734",
-          name: "Inglés 3 para Computación"
-        }];
+        vm.loaded = false;
+        vm.courses = [];
+        vm.searched = {};
+        vm.limit = 20;
+        vm.request = {
+          fields : "id,code,name"
+        };
+
+        vm.delete = deleteCourse;
+        vm.search = searchCourse;
+        vm.isEmpty = isEmpty;
+        vm.isLoaded = isLoaded;
+        vm.loadPage = loadPage;
+
+        activate();
+
+        function activate() {
+          var page = parseInt($location.search()['page']);
+
+          if (isNaN(page)) {
+            page = 1;
+          }
+
+          vm.totalPages = page;
+          vm.page = page;
+          loadPage();
+        }
+
+        function loadPage() {
+          $location.search('page', vm.page);
+
+          vm.request.page = vm.page;
+          vm.request.limit = vm.limit;
+
+          CourseService.GetAll(vm.request)
+          .then(setCourses)
+          .catch(handleError);
+        }
+
+        function searchCourse() {
+          vm.request.query = {};
+
+          if (vm.searched.code) {
+            vm.request.query.code = {
+              operation: "like",
+              value: '*' + vm.searched.code.replace(' ', '*') + '*'
+            }
+          }
+
+          if (vm.searched.name) {
+            vm.request.query.name = {
+              operation: "like",
+              value: '*' + vm.searched.name.replace(' ', '*') + '*'
+            }
+          }
+
+          loadPage();
+        }
+
+        function isEmpty() {
+          return vm.courses.length == 0;
+        }
+
+        function isLoaded() {
+          return vm.loaded;
+        }
+
+        function setCourses(data) {
+          vm.courses = data.results;
+          vm.page = data.current_page;
+          vm.totalPages = data.total_pages;
+          vm.totalItems = vm.limit * vm.totalPages;
+          vm.loaded = true;
+        }
+
+        function deleteCourse(id) {
+          MessageService.confirm("¿Desea realmente eliminar este curso?")
+          .then(function() {
+            CourseService.Delete(id)
+            .then(loadPage)
+            .catch(handleError);
+          });
+        }
+
+        function handleError(data) {
+          MessageService.error(data.description);
+        }
     }
 })();
