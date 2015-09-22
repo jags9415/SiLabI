8c35315fcbc2cf3b422ca19066d8c8cce8f3fc0e
Emmurillo <emmanuel-ms@hotmail.com>
2015-09-22 14:04:43 -0600

    Add software service to use it on Labs module

---
+++ frontend/app/scripts/operator/software/software.service.js
@@ -0,0 +1,50 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('silabi')
+    .service('SoftwareService', SoftwareService);
+
+  SoftwareService.$inject = ['RequestService', '$localStorage'];
+
+  function SoftwareService(RequestService, $localStorage) {
+    this.GetAll = GetAll;
+    this.GetOne = GetOne;
+    this.Update = Update;
+    this.Create = Create;
+    this.Delete = Delete;
+
+    function GetAll(request) {
+      if (!request) request = {};
+      request.access_token = $localStorage['access_token'];
+      return RequestService.get('/software', request);
+    }
+
+
+    function GetOne(SoftwareID) {
+      var request = {};
+      request.access_token = $localStorage['access_token'];
+      return RequestService.get('/software/' + SoftwareID, request);
+    }
+
+    function Update(SoftwareID, NewSoftwareInfo) {
+      var requestBody = {};
+      requestBody.laboratory = NewSoftwareInfo;
+      requestBody.access_token = $localStorage['access_token'];
+      return RequestService.put('/software/' + SoftwareID, requestBody);
+    }
+
+    function Create(Software) {
+      var requestBody = {};
+      requestBody.laboratory = Software;
+      requestBody.access_token = $localStorage['access_token'];
+      return RequestService.post('/software', requestBody);
+    }
+
+    function Delete(SoftwareID) {
+      var requestBody = {};
+      requestBody.access_token = $localStorage['access_token'];
+      return RequestService.delete('/software/' + SoftwareID, requestBody);
+    }
+  }
+})();
