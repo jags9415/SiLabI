b55b59073e075b2c4fd8a39f6634e390c774d009
Leonardo Madrigal <lmadrigal09@gmail.com>
2015-09-26 00:14:52 -0600

    Appointments' service added

---
+++ frontend/app/scripts/operator/appointments/appointment.service.js
@@ -0,0 +1,50 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('silabi')
+    .service('AppointmentService', AppointmentService);
+
+  AppointmentService.$inject = ['RequestService', '$localStorage'];
+
+  function AppointmentService(RequestService, $localStorage) {
+    this.GetAll = GetAll;
+    this.GetOne = GetOne;
+    this.Update = Update;
+    this.Create = Create;
+    this.Delete = Delete;
+
+    function GetAll(request) {
+      if (!request) request = {};
+      request.access_token = $localStorage['access_token'];
+      return RequestService.get('/appointments', request);
+    }
+
+
+    function GetOne(AppointmentID) {
+      var request = {};
+      request.access_token = $localStorage['access_token'];
+      return RequestService.get('/appointments/' + AppointmentID, request);
+    }
+
+    function Update(AppointmentID, NewAppointmentInfo) {
+      var requestBody = {};
+      requestBody.appointment = NewAppointmentInfo;
+      requestBody.access_token = $localStorage['access_token'];
+      return RequestService.put('/appointments/' + AppointmentID, requestBody);
+    }
+
+    function Create(Appointment) {
+      var requestBody = {};
+      requestBody.appointment = Appointment;
+      requestBody.access_token = $localStorage['access_token'];
+      return RequestService.post('/appointments', requestBody);
+    }
+
+    function Delete(AppointmentID) {
+      var requestBody = {};
+      requestBody.access_token = $localStorage['access_token'];
+      return RequestService.delete('/appointments/' + AppointmentID, requestBody);
+    }
+  }
+})();
