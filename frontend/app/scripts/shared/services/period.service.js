83791fbabbb615e5530538a9ccc887fda035d239
Leonardo Madrigal <lmadrigal09@gmail.com>
2015-10-11 00:45:44 -0600

    Period service update.

--- frontend/app/scripts/shared/services/period.service.js
+++ frontend/app/scripts/shared/services/period.service.js
@@ -9,6 +9,7 @@
 
     function PeriodService($q) {
       this.GetAll = GetAll;
+      this.GetCurrentPeriod = GetCurrentPeriod;
 
       function GetAll() {
         var defer = $q.defer();
@@ -75,5 +76,114 @@
         defer.resolve(periods);
         return defer.promise;
       }
+
+    function GetCurrentPeriod(periodType)
+    {
+      var currentYear = new Date().getFullYear();
+      var res = {};
+      switch(periodType)
+      {
+        case 'Semestre':
+          res = 
+          {
+            value: getSemester(),
+            year: currentYear
+          };
+          break;
+        case 'Cuatrimestre':
+          res = 
+          {
+            value: getQuarter(),
+            year: currentYear
+          };
+          break;
+        case 'Trimestre':
+          res = 
+          {
+            value: getTrimester(),
+            year: currentYear
+          };
+          break;
+        case 'Bimestre':
+          res = 
+          {
+            value: getBimester(),
+            year: currentYear
+          };
+      }
+      return res;
+    }
+
+    function getSemester () 
+    {
+      var month = new Date().getMonth();
+      var res = 1;
+      if(month > 5) res = 2;
+      return res;
+    }
+
+    function getQuarter() {
+      var month = new Date().getMonth();
+      var res = 0;
+      switch(month)
+      {
+        case month > 3 && month <=7:
+          res = 2;
+          break;
+        case month > 7 && month <=11:
+          res = 3;
+          break;
+        default:
+          res = 1;
+      }
+      return res;
+    }
+
+    function getTrimester() 
+    {
+      var month = new Date().getMonth();
+      var res = 0;
+      switch(month)
+      {
+        case month > 2 && month <=5:
+          res = 2;
+          break;
+        case month > 5 && month <=8:
+          res = 3;
+          break;
+        case month > 8 && month <=11:
+          res = 4;
+          break;
+        default:
+          res = 1;
+      }
+    }
+
+    function getBimester() 
+    {
+      var month = new Date().getMonth();
+      var res = 0;
+      switch(month)
+      {
+        case month > 1 && month <=3:
+          res = 2;
+          break;
+        case month > 3 && month <=5:
+          res = 3;
+          break;
+        case month > 5 && month <=7:
+          res = 4;
+          break;
+        case month > 7 && month <=9:
+          res = 5;
+          break;
+        case month > 9 && month <=11:
+          res = 6;
+          break;
+        default:
+          res = 1;
+      }
+      return res;
+    }
     }
 })();
