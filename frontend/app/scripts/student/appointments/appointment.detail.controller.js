899351768f3a4cc0e3fdf30f64e7c21c98575038
Emmurillo <emmanuel-ms@hotmail.com>
2015-10-12 18:16:31 -0600

    Update student appointment's detail view

--- frontend/app/scripts/student/appointments/appointment.detail.controller.js
+++ frontend/app/scripts/student/appointments/appointment.detail.controller.js
@@ -5,93 +5,171 @@
       .module('silabi')
       .controller('StudentAppDetailController', AppointmentDetailController);
 
-  AppointmentDetailController.$inject = ['StudentAppService', 'AppointmentDateService', 'MessageService', 'StudentService', 'SoftwareService', '$routeParams', '$localStorage'];
+  AppointmentDetailController.$inject = ['StudentAppService', 'AppointmentDateService', 'MessageService', 'SoftwareService', '$localStorage', '$location', '$routeParams'];
 
-function AppointmentDetailController(StudentAppService, AppointmentDateService, MessageService, StudentService, SoftwareService, $routeParams, $localStorage) {
+  function AppointmentDetailController(StudentAppService, AppointmentDateService, MessageService, SoftwareService, $localStorage, $location, $routeParams) {
+    var vm = this;
+    vm.selected_software = {};
+    vm.software_list = [];
+    vm.available_dates = [];
+    vm.available_hours = [];
+    vm.selected_state = {};
+    vm.selected_laboratory = {};
+    vm.$storage = $localStorage;
 
-  var vm = this;
-  vm.software = {};
-  vm.software_list = [];
-  vm.courses = [];
-  vm.states = [];
-  vm.$storage = $localStorage;
+    vm.request = {
+      fields: "id,date,state,created_at,updated_at,group.number,group.course.name,software.code,software.name,laboratory.name"
+    }
 
-  vm.update = updateAppointment;
+    vm.date_request = {
+      fields: "date,laboratory.name"
+    };
 
-  activate();
+    vm.software_request = {
+      fields: "code,name",
+      limit: 10
+    };
 
-  function activate() {
-    vm.student_id = vm.$storage['username'];
-    vm.app_id = $routeParams.app_id;
-    getAppointment();
-    getSoftware();
+    vm.setAvailableHours = setAvailableHours;
+    vm.changeLaboratory = changeLaboratory;
+    vm.searchSoftware = searchSoftware;
+    vm.setSoftware = setSoftware;
+    vm.update = updateAppointment;
+    vm.delete = deleteAppointment;
+
+    activate();
+
+    function activate() {
+      vm.student_id = vm.$storage['username'];
+      vm.app_id = $routeParams.app_id;
+      getAppointment();
+    }
+
+    function getAppointment () {
+      StudentAppService.GetOne(vm.student_id, vm.app_id, vm.request)
+      .then(setAppointment)
+      .then(setAvailableDates)
+      .catch(handleError);
+    }
+
+    function searchSoftware (input) {
+      vm.software_request.query = {};
+
+      vm.software_request.query.code = {
+        operation: "like",
+        value: '*' + input + '*'
+      }
+
+      return SoftwareService.GetAll(vm.software_request)
+        .then(function(data) {
+          return data.results;
+        });
+    }
+
+    function getAvailableDates() {
+        return AppointmentDateService.GetAvailable(vm.date_request, vm.student_id);
+    }
+
+    function setAppointment (data) {
+      vm.appointment = data;
+      return getAvailableDates();
+    }
+
+    function setSoftware (data) {
+      vm.appointment.software = data;
+    }
+
+    function setAvailableDates (dates) {
+      vm.available_dates = AppointmentDateService.ParseAvailableDates(dates);
+      vm.selected_date = null;
+
+      for (var i = 0; i < vm.available_dates.length; i++) {
+        if (moment(vm.available_dates[i].day).isSame(vm.appointment.date, 'day')) {
+          vm.selected_date = vm.available_dates[i];
+          setAvailableHours();
+          break;
+        }
+      }
+
+      if (!vm.selected_date) {
+        var current = {
+          day: moment(vm.appointment.date).format("YYYY-MM-DD"),
+          hoursByLab: [{
+            full_date: vm.appointment.date,
+            hour: moment(vm.appointment.date).format("HH:mm"),
+            laboratory: vm.appointment.laboratory
+          }]
+        };
+
+        vm.available_dates.unshift(current);
+        vm.selected_date = vm.available_dates[0];
+        setAvailableHours();
+      }
+    }
+
+    function setAvailableHours() {
+      if (vm.selected_date) {
+        vm.available_hours = vm.selected_date.hoursByLab;
+        vm.selected_hour = null;
+
+        for (var i = 0; i < vm.available_hours.length; i++) {
+          if (moment(vm.available_hours[i].full_date).isSame(vm.appointment.date, 'hour')) {
+            vm.selected_hour = vm.available_hours[i];
+            break;
+          }
+        }
+
+        if (!vm.selected_hour) {
+          vm.selected_hour = vm.available_hours[0];
+        }
+      }
+    }
+
+    function changeLaboratory () {
+      if (vm.selected_hour) {
+        vm.selected_laboratory = vm.selected_hour.laboratory;
+      }
+    }
+
+    function updateAppointment () {
+      if (!_.isEmpty(vm.appointment)) {
+        if (vm.selected_date && vm.selected_hour) {
+          vm.date = vm.selected_date.day + "T" + vm.selected_hour.hour + ":00.000";
+        }
+
+        var app = {
+          "date": vm.date,
+          "software": vm.appointment.software.code
+        }
+
+        StudentAppService.Update(vm.student_id, vm.app_id, app)
+        .then(handleSuccess)
+        .catch(handleError);
+      }
+    }
+
+    function deleteAppointment() {
+      if (!_.isEmpty(vm.appointment)) {
+        MessageService.confirm("¿Desea realmente eliminar esta cita?")
+        .then(function() {
+          StudentAppService.Delete(vm.student_id, vm.app_id)
+          .then(redirectPage)
+          .catch(handleError);
+        });
+      }
+    }
+
+    function redirectPage () {
+      $location.path("/Estudiante/Citas");
+    }
+
+    function handleSuccess (data) {
+      MessageService.success("Cita actualizada.");
+      setAppointment(data);
+    }
+
+    function handleError(data) {
+      MessageService.error(data.description);
+    }
   }
-
-  function getAppointment () {
-    StudentAppService.GetOne(vm.student_id, vm.app_id)
-    .then(setAppointment)
-    .catch(handleError);
-  }
-
-function getSoftware () {
-
-  SoftwareService.GetAll()
-  .then(setSoftware)
-  .catch(handleError);
-}
-
-function getStates () {
-StateService.GetAppointmentStates()
-  .then(setStates)
-  .catch(handleError);
-}
-
-function setStates(states) {
-  vm.states = states;
-}
-
-function setAppointment (data) {
-  console.log(data);
-  vm.appointment = data;
-  vm.student = data.student;
-  vm.date = data.date;
-  vm.laboratory = data.laboratory;
-  vm.software = data.software;
-  var i = data.date.indexOf("T");
-  var date_day = data.date.substring(0, i);
-  var date_hour = data.date.substring(i+1, data.date.length - 7);
-  vm.hour = date_hour;
-}
-
-function setLaboratories (data) {
-  vm.laboratories = data.results;
-}
-
-function setSoftware(data) {
-vm.software_list = data.results;
-}
-
-function updateAppointment () {
-  var date = new Date(vm.date.getFullYear(), vm.date.getMonth(), vm.date.getUTCDate(), vm.hour.slice(0, 2));
-  console.log(date.toJSON());
-  var app =
-  {
-    "laboratory": vm.laboratory.name,
-    "software": vm.software.code,
-    "date": date.toJSON()
-  }
-  StudentAppService.Update(vm.student_id, vm.app_id, app)
-  .then(handleSuccess)
-  .catch(handleError);
-}
-
-function handleSuccess (data) {
-  MessageService.success("Cita actualizada con éxito.");
-  setAppointment(data);
-}
-
-function handleError(data) {
-  MessageService.error(data.description);
-}
-}
 })();
