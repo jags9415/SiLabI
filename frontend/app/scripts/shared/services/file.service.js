c9f6ef5f4125fee702f9ca14855939e50b80aaec
Leonardo Madrigal <lmadrigal09@gmail.com>
2015-10-19 12:09:03 -0600

    Minor update in file service.

--- frontend/app/scripts/shared/services/file.service.js
+++ frontend/app/scripts/shared/services/file.service.js
@@ -9,7 +9,7 @@
 
   function FileService(RequestService, $localStorage) {
 
-    function appointmentsByStudentToPDF (apps) {
+    function appointmentsByStudentToPDF (apps, htmlID) {
       var doc = new jsPDF();
 
       var specialElementHandlers = {
@@ -17,35 +17,6 @@
           return true;
         }
       };
-
-      var html = document.createElement("DIV");
-      var table = 
-      "<table class='table table-striped'>
-      <thead>
-        <tr>
-          <th>Fecha</th>
-          <th>Software</th>
-          <th>Laboratorio</th>
-        </tr>
-      </thead>
-      <tbody>
-      </tbody>
-      </table>";
-
-      var tbody = table.getElementsByTagName('tbody');
-
-      for (var i = 0; i < apps.length 0; i++) 
-      {
-        var newRow = tbody.insertRow(tbody.rows.length);
-        var lab = newRow.insertCell(0);
-        var software = newRow.insertCell(0);
-        var date = newRow.insertCell(0);
-        lab.innerHTML = apps[i].laboratory.name;
-        software.innerHTML = apps[i].software.code;
-        date.innerHTML = apps[i].date;
-      }
-      table.getElementsByTagName('tbody') = tbody;
-      html.appendChild(table);
 
       doc.fromHTML(html, 15, 15, {
         'width': 170, 
