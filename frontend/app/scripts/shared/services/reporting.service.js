dc7dd43ed0d178a32c3faaaf034ac5c90be84098
Leonardo Madrigal <lmadrigal09@gmail.com>
2015-10-20 10:10:11 -0600

    Reporting service update.

--- frontend/app/scripts/shared/services/reporting.service.js
+++ frontend/app/scripts/shared/services/reporting.service.js
@@ -10,10 +10,12 @@
   function ReportingService(RequestService, $localStorage) {
 
     var appointmentsByStudentRquest = null;
+    var appointmentsByGroupRquest = null;
     this.setAppointmentsByStudentRequest = setAppointmentsByStudentRequest;
     this.getAppointmentsByStudentRequest = getAppointmentsByStudentRequest;
-    this.GetAppointmentsByStudent = GetAppointmentsByStudent;
-    this.GetAppointmentsByGroup = GetAppointmentsByGroup;
+    this.setAppointmentsByGroupRequest = setAppointmentsByGroupRequest;
+    this.getAppointmentsByGroupRequest = getAppointmentsByGroupRequest;
+    this.GetAppointments = GetAppointments;
 
     function setAppointmentsByStudentRequest (request) {
       appointmentsByStudentRquest = request;
@@ -23,57 +25,17 @@
       return appointmentsByStudentRquest;
     }
 
-    function GetAppointmentsByStudent(request) {
+    function setAppointmentsByGroupRequest (request) {
+      appointmentsByGroupRquest = request;
+    }
+
+    function getAppointmentsByGroupRequest () {
+      return appointmentsByGroupRquest;
+    }
+
+    function GetAppointments(request) {
       request['access_token'] = $localStorage['access_token'];
       return RequestService.get('/appointments', request);
     }
-
-    function GetAppointmentsByGroup(Group, startDate, endDate) {
-      var result = [];
-      var group_request = {
-        fields: "username,full_name",
-        limit: -1,
-        localStorage: $localStorage['access_token']
-      };
-
-      var student_request = {
-        fields: "id,date,software.code,software.name",
-        limit: -1,
-        localStorage: $localStorage['access_token'],
-        query: {
-          'date':[
-            {
-              operation: 'ge',
-              value: startDate
-            },
-            {
-              operation: 'lt',
-              value: endDate
-            }
-          ]
-        }
-      };
-
-      var students = RequestService.get('/groups/' + Group.id + '/students', group_request);
-
-      for (var i = 0; i < students.length; i++) {
-        var current_student = students[i];
-        student_request.query['student_request'] ={
-          operation: "eq",
-          value:current_student.username
-        };
-        var apps = GetAppointmentsByStudent(student_request);
-        var json = {
-          student_name: current_student.full_name,
-          username: current_student.username,
-          appointments: apps
-        };
-
-        result.push(json);
-      }
-      
-      return result;
-    }
-
   }
 })();
